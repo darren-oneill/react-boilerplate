81bb0549acc0b9f1685382f4b14f1ca235252fee
Darren ONeill <webdevdarren@gmail.com>
2019-07-30 11:58:36 +1200

    Updating Readme and removing logging

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # react-boilerplate
-React Boilerplate using Parcel, ESLint, Prettier, Styled Components and Reach Router
+
+A minimal React boilerplate, to get you up and running asap, using:
+
+Parcel<br>
+ESLint<br>
+Prettier<br>
+Reach Router<br>
+Styled Components<br>
+SVG's as React Components<br>
+
+If you want to get the NPM modules installed, just `cd` into the project directory (react-boilerplate) and `yarn`
+
+To get the dev server up and running `yarn dev`
--- src/styling/mqs.js
+++ src/styling/mqs.js
@@ -4,17 +4,14 @@ import { BREAK_POINTS } from "./globalValues";
 // Create media queries unsing Styled Components
 const mqs = Object.keys(BREAK_POINTS).reduce((accumulator, label) => {
   const lowercaseLabel = label.toLowerCase();
-  console.log(label);
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
   const emSize = BREAK_POINTS[label] / 16;
-  console.log(emSize);
   accumulator[lowercaseLabel] = (...args) => css`
     @media (min-width: ${emSize}em) {
       ${css(...args)};
     }
   `;
-  console.log(accumulator);
   return accumulator;
 }, {});
 
